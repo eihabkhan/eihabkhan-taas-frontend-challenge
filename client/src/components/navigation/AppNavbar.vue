e31809cfa8452399c1c582ecc3c9130a74c3d312
Eihab Khan <mastereihab@gmail.com>
2023-08-14 12:05:41 +0100

    add user profile image

--- client/src/components/navigation/AppNavbar.vue
+++ client/src/components/navigation/AppNavbar.vue
@@ -1,10 +1,16 @@
 <script lang="ts">
 import CommitlyLogo from '../base/CommitlyLogo.vue'
+import UserProfile from '../profile/UserProfile.vue'
 import { defineComponent } from 'vue'
 export default defineComponent({
-  components: {
-    CommitlyLogo,
+  props: {
+    name: String,
+    imageUrl: {
+      type: String,
+      default: '/images/profile-placeholder.png',
+    },
   },
+  components: { CommitlyLogo, UserProfile },
 })
 </script>
 
@@ -12,6 +18,7 @@ export default defineComponent({
   <header class="mt-10 w-full container max-w-[1024px]">
     <div class="flex justify-between items-center">
       <CommitlyLogo />
+      <UserProfile />
     </div>
   </header>
 </template>
---
+++ client/src/components/profile/UserProfile.vue
@@ -0,0 +1,24 @@
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      name: 'John Doe',
+      imageUrl: '/images/profile-placeholder.png',
+    }
+  },
+})
+</script>
+
+<template>
+  <div class="flex items-center justify-center text-black gap-4">
+    <span>{{ name }}</span>
+    <img
+      v-if="imageUrl"
+      class="h-12 w-12 rounded-full"
+      :src="imageUrl"
+      alt=""
+    />
+  </div>
+</template>
